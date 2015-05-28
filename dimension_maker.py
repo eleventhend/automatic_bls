3406fe6f488785a134011ce1d0637c71cf01bc1f
Jeff Ferguson <jferguson51@gmail.com>
2015-05-28 10:16:44 -0400

    adding dbs and support for them

--- dimension_maker.py
+++ dimension_maker.py
@@ -8,12 +8,6 @@ def dim_maker(df, engine, output_table):
     pd.DataFrame.to_sql(df, con=engine, name=output_table, 
         if_exists='append', index=False)
 
-    #dd_statement = "CREATE TABLE dedupe as SELECT * FROM %(opt)s WHERE 1 "
-    #" GROUP BY %(col_zero)s, %(col_one)s;" % {"opt": output_table, "col_zero":\
-    # df.columns[0], "col_one": df.columns[1]}
-    #drop_statement = "DROP TABLE %(opt)s;" % {"opt": output_table}
-    #rename_statement = "RENAME TABLE dedupe TO %(opt)s;" % {"opt": output_table}
-
     engine.execute("CREATE TABLE dedupe as SELECT * FROM %(opt)s WHERE 1 "
     " GROUP BY %(col_zero)s, %(col_one)s;" % {"opt": output_table, "col_zero":\
      df.columns[0], "col_one": df.columns[1]})
@@ -37,6 +31,13 @@ for x in range(0, len(pre_df.index), 1):
         converters={'measure_code': lambda x: str(x)})
     geo_df['db_prefix'] = p
     mc_df['db_prefix'] = p
+    try:
+        sector_df = pd.read_csv(p + "sector_codes.csv",
+            converters={'sector_code': lambda x: str(x)})
+        sector_df['db_prefix'] = p
+        dim_maker(sector_df, engine, "sector_df")
+    except:
+        pass
     dim_maker(geo_df, engine, "dim_geo")
     dim_maker(mc_df, engine, "dim_mc")
 

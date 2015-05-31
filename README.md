c9b497e61e148c04e89c40f3cba02f340bde4b5e
Jeff Ferguson <jferguson51@gmail.com>
2015-05-31 17:04:18 -0400

    various changes; multi-db population

--- README.md
+++ README.md
@@ -6,6 +6,17 @@ Automatic_bls.py does the following:
 1. Query api based on series read from csv files
 2. Send api data to sql database, updating and archiving deprecated data
 
+
+"""
+1 - Empties out the incubator table
+2 - Sends requests to BLS API for all prefixes listed in prefix.csv
+    2.1 - Takes series components from respective local folders
+    2.2 - Outputs to SQL incubator table using sqlalchemy engine
+3 - Inserts the incubator table into the fact table
+    3.1 - Sends deprecated data to archive table
+    3.2 - Updates fact table with new data
+"""
+
 ######Note that debugging prints & timers are still in to gauge how the API is handling requests & timeouts
 
 #####Requires command line argument of desired database prefix. Current arguments include:
@@ -37,3 +48,4 @@ http://data.bls.gov/registrationEngine/
 #####sql_engine.txt
 The sqlalchemy address to your sql database:
 http://docs.sqlalchemy.org/en/latest/core/engines.html
+
--- dimension_maker.py
+++ dimension_maker.py
@@ -8,9 +8,8 @@ def dim_maker(df, engine, output_table):
     pd.DataFrame.to_sql(df, con=engine, name=output_table, 
         if_exists='append', index=False)
 
-    engine.execute("CREATE TABLE dedupe as SELECT * FROM %(opt)s WHERE 1 "
-    " GROUP BY %(col_zero)s, %(col_one)s;" % {"opt": output_table, "col_zero":\
-     df.columns[0], "col_one": df.columns[1]})
+    engine.execute("CREATE TABLE dedupe as SELECT DISTINCT * FROM %(opt)s;"\
+     % {"opt": output_table})
     engine.execute("DROP TABLE %(opt)s;" % {"opt": output_table})
     engine.execute("RENAME TABLE dedupe TO %(opt)s;" % {"opt": output_table})
     return
@@ -25,20 +24,24 @@ pre_df = pd.read_csv("prefix.csv")
 dim_maker(pre_df, engine, "dim_prefix")
 
 for x in range(0, len(pre_df.index), 1):
-    p = pre_df['prefix'].iloc[0]
-    geo_df = pd.read_csv(p + "/geo_codes.csv")
+    p = pre_df['prefix'].iloc[x]
+    geo_df = pd.read_csv(p + "/geo_codes.csv",
+        converters={'area_code': lambda x: str(x)})
     mc_df = pd.read_csv(p + "/measure_codes.csv",
         converters={'measure_code': lambda x: str(x)})
+    seasonal_df = pd.read_csv(p + "/seasonal_codes.csv")
     geo_df['db_prefix'] = p
     mc_df['db_prefix'] = p
+    seasonal_df['db_prefix'] = p
     try:
-        sector_df = pd.read_csv(p + "sector_codes.csv",
+        sector_df = pd.read_csv(p + "/sector_codes.csv",
             converters={'sector_code': lambda x: str(x)})
         sector_df['db_prefix'] = p
         dim_maker(sector_df, engine, "sector_df")
     except:
         pass
     dim_maker(geo_df, engine, "dim_geo")
-    dim_maker(mc_df, engine, "dim_mc")
+    dim_maker(mc_df, engine, "dim_measure_codes")
+    dim_maker(seasonal_df, engine, "dim_seasonal")
 
 print "to the moon!"

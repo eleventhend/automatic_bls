5c3aac8ebe99c901d28bad8118a274825196521a
Jeff Ferguson <jferguson51@gmail.com>
2015-06-10 16:41:57 -0400

    something is wrong with auto_incrementing

--- dimension_maker.py
+++ dimension_maker.py
@@ -3,9 +3,9 @@ import csv
 from sqlalchemy import create_engine
 import pandas as pd
 
-def dim_add_dedupe(df, engine, output_table):
+def add_dedupe(df, engine, output_table):
 
-    pd.DataFrame.to_sql(df, con=engine, name=output_table, 
+    pd.DataFrame.to_sql(df, name=output_table, con=engine, 
         if_exists='append', index=False)
 
     engine.execute("CREATE TABLE dedupe as SELECT DISTINCT * FROM %(opt)s;"
@@ -22,28 +22,35 @@ with open ('sql_engine.txt', 'r') as f:
     engine_address = f.read()
 engine = create_engine(engine_address)
 
-engine.execute("CREATE TABLE IF NOT EXISTS dim_series (`id` int(11) NOT NULL \
-    AUTO_INCREMENT, `series` varchar(64), `prefix` enum('LA','SM','EN'), \
-    `prefix_long` enum('LAUS', 'CES', 'QCEW'), `seasonal_code` enum('S','U'), \
+engine.execute("CREATE TABLE IF NOT EXISTS dimtest_series (`id` int(11) NOT NULL \
+    AUTO_INCREMENT, `series` varchar(64), `prefix` varchar(2), \
+    `prefix_long` varchar(6), `seasonal_code` enum('S','U'), \
     `seasonal_desc` text, `area_code` varchar(32), `area_code_id` int, \
     `sector_code` varchar(16), `sector_code_id` int, \
-    `measure_code` varchar(4), `measure_code_id` int \
-    PRIMARY KEY (id))")
+    `measure_code` varchar(4), `measure_code_id` int, \
+    PRIMARY KEY (`id`))")
 
-engine.execute("CREATE TABLE IF NOT EXISTS dim_measure_codes (`id` int \
+engine.execute("CREATE TABLE IF NOT EXISTS dimtest_measure_codes (`id` int \
     NOT NULL AUTO_INCREMENT, `measure_code` varchar(2), `measure_text` text, \
-    `prefix` enum('LA','SM', 'EN'), PRIMARY KEY (id))")
+    `prefix` varchar(2), PRIMARY KEY (id))")
 
-engine.execute("CREATE TABLE IF NOT EXISTS dim_geo (`id` int NOT NULL \
+engine.execute("CREATE TABLE IF NOT EXISTS dimtest_geo (`id` int NOT NULL \
     AUTO_INCREMENT, `area_type_code` varchar(1), `area_code` varchar(32), \
     `area_text` varchar(64), `state` varchar(2), `prefix` \
-    enum('LA','SM', 'EN'), PRIMARY KEY (id))")
+    varchar(2), PRIMARY KEY (`id`))")
 
-engine.execute("CREATE TABLE IF NOT EXISTS dim_sector (`id` int NOT NULL \
+"""sector needs to have a none row"""
+
+engine.execute("CREATE TABLE IF NOT EXISTS dimtest_sector (`id` int NOT NULL \
     AUTO_INCREMENT, `sector_code` varchar(16), `sector_name` varchar(128), \
-    `prefix` enum('LA','SM', 'EN'), PRIMARY KEY (id))")
+    `prefix` varchar(2), PRIMARY KEY (`id`))")
 
 prefix = pd.read_csv("prefix.csv")
+
+df_empty = pd.DataFrame(index = [0], columns = ['series', 
+    'prefix', 'prefix_long', 'seasonal_code', 'seasonal_desc', 
+    'area_code', 'sector_code', 
+    'measure_code', 'measure_code'])
 
 for x in range(0, len(prefix.index), 1):
     p = prefix['prefix'].iloc[x]
@@ -56,36 +63,41 @@ for x in range(0, len(prefix.index), 1):
         sector = pd.read_csv(p + "/sector_codes.csv",
             converters={'sector_code': lambda x: str(x)})
         sector['prefix'] = p
-        dim_add_dedupe(sector, engine, "dim_sector")
+        add_dedupe(sector, engine, "dimtest_sector")
     except:
+        sector_none = pd.DataFrame(index = [0], columns = ['sector_code' 
+            'sector_name', 'prefix'])
+        sector_none['sector_code'] = "none"
+        sector_none['sector_name'] = "default no sector"
+        sector_none['prefix'] = p
+        #add_dedupe(sector_none, engine, "dimtest_sector")
         sector = pd.DataFrame(index = [0], columns = ['sector_code'])
         sector = sector.fillna('')
     for x in range(0, len(seasonal.index), 1):
         for y in range(0, len(m_c.index), 1):
             for z in range(0, len(sector.index), 1):
-                df_sql = pd.DataFrame(index = [0], columns = ['series', 
-                    'prefix', 'prefix_long', 'seasonal_code', 'seasonal_desc', 
-                    'area_code', 'sector_code', 'measure_code', 'measure_code'])
+                df_sql = df_empty
                 for i in range(0, len(geo.index), 1):
                     ser_concat = (p + seasonal['seasonal_code'].iloc[x] + 
                         geo['area_code'].iloc[i] + 
                         sector['sector_code'].iloc[z] + 
                         m_c['measure_code'].iloc[y])
-                    df_sql = pd.DataFrame(index = [0], columns = ['series', 
-                        'prefix', 'prefix_long', 'seasonal_code', 'seasonal_desc', 
-                        'area_code', 'sector_code', 'measure_code', 'measure_code'])
+                    df_con = df_empty
                     df_con['series'] = ser_concat
                     df_con['area_code'] = geo['area_code'].iloc[i]
                     df_sql = df_sql.append(df_con, True)
                 df_sql['prefix'] = p 
-                df_sql['sector_code'] = sector['sector_code'].iloc[z]
+                if sector['sector_code'].iloc[z] == '':
+                    df_sql['sector_code'] = 'none'
+                else:
+                    df_sql['sector_code'] = sector['sector_code'].iloc[z]
                 df_sql['seasonal_code'] = seasonal['seasonal_code'].iloc[x]
                 df_sql['measure_code'] = m_c['measure_code'].iloc[y]
                 df_sql = df_sql.dropna()
-                dim_add_dedupe(df_sql, engine, "dim_series")
+                add_dedupe(df_sql, engine, "dimtest_series")
     geo['prefix'] = p
     m_c['prefix'] = p
     seasonal['prefix'] = p
-    dim_add_dedupe(geo, engine, "dim_geo")
-    dim_add_dedupe(m_c, engine, "dim_measure_codes")
-    dim_add_dedupe(seasonal, engine, "dim_seasonal")
+    add_dedupe(geo, engine, "dimtest_geo")
+    add_dedupe(m_c, engine, "dimtest_measure_codes")
+    add_dedupe(seasonal, engine, "dimtest_seasonal")

bbc0905076e006ef285f9e8748ca3f4c2c7e7abd
Jeff Ferguson <eleventhend@gmail.com>
2015-08-11 15:51:49 -0400

    added GNU license

--- dimension_maker.py
+++ dimension_maker.py
@@ -1,3 +1,24 @@
+"""
+dimension_maker.py:
+1. Concatenates local files of series components into full series codes
+2. Sends full and partial series codes to SQL database
+"""
+
+#Copyright (C) 2015 Jeff Ferguson <jferguson51 AT gmail DOT com>
+
+#This program is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, see <http://www.gnu.org/licenses/>.
+
 import csv
 
 from sqlalchemy import create_engine
